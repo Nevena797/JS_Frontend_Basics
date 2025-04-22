288b9734e62e3e4254ff551082bba81545e89647
Nevena <nevena.rogova@gmx.at>
2025-04-22 10:36:14 +0300

    Add files via upload

--- 02. JS_Syntax_Fundamentals_exer/01.Ages.js
+++ 02. JS_Syntax_Fundamentals_exer/01.Ages.js
@@ -1,4 +1,17 @@
-function solve(age){
-    
+function ages(number) {
+    if (number < 0) {
+        console.log("out of bounds");
+    } else if (number >= 0 && number < 3) {
+        console.log("baby");
+    } else if (number >= 3 && number <= 13) {
+        console.log("child");
+    } else if (number >= 14 && number < 20) {
+        console.log("teenager");
+    } else if (number >= 20 && number <= 65) {
+        console.log("adult");
+    } else if (number >= 66) {
+        console.log("elder");
+    } else {
+        console.log("out of bounds");
+    }
 }
-solve(20);
